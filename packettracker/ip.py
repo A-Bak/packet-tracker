f621513857f08ed5b8adf1cd07f0fdc9946ff46d
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-05-18 13:15:30 +0200

    Added - getting public IP from ipinfo.io API.

--- packettracker/ip.py
+++ packettracker/ip.py
@@ -1,5 +1,7 @@
-from typing import Tuple
 from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+
+import requests
 
 
 
@@ -15,4 +17,30 @@ class IP:
     
     address: str    
     location: Location = field(init=False, default=None)
+    
 
+class StatusCodeError(ConnectionError):
+    """ Unexpected status code exception. """
+    
+    
+class PublicIPAddressAPI(ABC):
+    
+    @classmethod
+    @abstractmethod
+    async def get_public_ip(cls) -> IP:
+        ...
+        
+
+class IPInfoAPI(PublicIPAddressAPI):
+    
+    @classmethod
+    async def get_public_ip(cls) -> IP:
+        
+        response = requests.get('https://ipinfo.io/json', verify=True)
+
+        if response.status_code != 200:
+            raise StatusCodeError('Failed to retrieve public ip information.')
+        
+        return IP(response.json()['ip'])
+
+   
